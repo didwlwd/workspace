0ec243f238ad72003d524ee3dfc07f67f0a9b1ed
didwlwd <didehdals321@naver.com>
2025-04-18 13:47:48 +0900

    react_component

--- 10_React/react-project/src/App.jsx
+++ 10_React/react-project/src/App.jsx
@@ -1,16 +1,10 @@
 import './App.css'
-import JavaScript from './components/JavaScript';
-import Style from './components/Style'
-import Products from './components/Products';
 import Hello from './components/Hello';
 import Heading from './components/Heading';
 
 function App() {
   return (
     <>
-      {/*<JavaScript/>*/}
-      {/* <Style/> */}
-      {/* <Products/> */}
       <Heading type="h2"/>
       <Heading/>
       <Heading>
