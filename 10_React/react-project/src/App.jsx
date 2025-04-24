c24a4a3443965bdad75e036fa12ce7d8f67bac6e
didwlwd <didehdals321@naver.com>
2025-04-24 13:35:59 +0900

    main

--- 10_React/react-project/src/App.jsx
+++ 10_React/react-project/src/App.jsx
@@ -3,25 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import UseStateTest from './components/useState/UseStateTest'
-import SignUp from './components/useState/SignUp'
-import LandingPage from './components/useState/LandingPage'
-import UseRefTest from './components/useRef/UseRefTest'
-import UseRefScroll from './components/useRef/UseRefScroll'
-import UseMemoTest from './components/useMemo/UseMemoTest'
-import UseCallbackTest from './components/useCallback/UseCallbackTest'
-
 function App() {
 
   return (
     <>
-      {/* <UseStateTest /> */}
-      {/* <SignUp /> */}
-      {/* <LandingPage /> */}
-      {/* <UseRefTest /> */}
-      {/* <UseRefScroll /> */}
-      <UseMemoTest />
-      {/* <UseCallbackTest /> */}
+      
     </>
   )
 }
--- 10_React/react-project/src/components/useCallback/ViewItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ViewItem = () => {
-  return (
-    <div>ViewItem</div>
-  )
-}
-
-export default ViewItem
