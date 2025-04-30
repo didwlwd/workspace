5ef3cd5b162092ca87524dc32d71a2bdf6483060
didwlwd <didehdals321@naver.com>
2025-04-30 17:01:31 +0900

    08_react

--- 10_React/react-project/src/App.jsx
+++ 10_React/react-project/src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import IconButton from './components/IconButton';
+import GlobalStyle from './GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from './theme';
+import ThemeBox from './components/ThemeBox';
+import { toast, ToastContainer } from 'react-toastify';
+import { performToast } from './utils/performToast';
+import SimpleForm from './components/SimpleForm';
+import LoaderDemo from './components/LoaderDemo';
+import TodoList from './components/TodoList';
+
+performToast({ msg: '요청에 성공하였습니다.', type: 'success' });
 
 function App() {
+  const [isDark, setIsDark] = useState(false);
+  const toggleTheme = () => setIsDark(!isDark);
+
+  const apiUrl = import.meta.env.VITE_API_URL;
+  console.log(apiUrl);
 
   return (
     <>
-      
+      {/* <IconButton /> */}
+      {/* <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+        <GlobalStyle />
+        <ThemeBox onToggleTheme={toggleTheme} />
+      </ThemeProvider> */}
+      {/* <ToastContainer /> */}
+      {/* <SimpleForm /> */}
+      {/* <LoaderDemo /> */}
+      <TodoList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
